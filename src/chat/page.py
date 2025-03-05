f3322173b66ae3390c3d39e33ae17ac44ad0c1db
jfsen <47889681+jfsen@users.noreply.github.com>
2025-03-05 13:35:22 +0100

    Add browser button to chat page for opening streams in browser

--- src/chat/page.py
+++ src/chat/page.py
@@ -9,6 +9,7 @@ from .irc_client import TwitchIRCClient, ConnectionState
 from .emote_service import EmoteService
 from .emote_store import EmoteStore
 from .emote_renderer import EmoteRenderer
+from streamline.icon_names import IconNames
 
 class ChatPage(Adw.NavigationPage):
     def __init__(self, streamer: str):
@@ -56,6 +57,7 @@ class ChatPage(Adw.NavigationPage):
         self.header = Adw.HeaderBar()
         self.header.add_css_class("flat")
         
+        # Status button (left side)
         self.status_button = Gtk.Button()
         self.status_button.add_css_class("flat")
         self.status_button.add_css_class("circular")
@@ -63,12 +65,20 @@ class ChatPage(Adw.NavigationPage):
         self._set_connection_status(False)
         self.header.pack_start(self.status_button)
         
+        # Play button (right side)
         self.play_button = Gtk.Button(icon_name="media-playback-start-symbolic")
         self.play_button.add_css_class("flat")
         self.play_button.set_tooltip_text("Play this stream")
         self.play_button.connect("clicked", self._on_play_clicked)
         self.header.pack_end(self.play_button)
-        
+
+        # Browser button (right side, before play button)
+        self.browser_button = Gtk.Button(icon_name=IconNames.BROWSER)
+        self.browser_button.add_css_class("flat")
+        self.browser_button.set_tooltip_text("Open in browser")
+        self.browser_button.connect("clicked", self._on_browser_clicked)
+        self.header.pack_end(self.browser_button)
+
         # Setup chat view
         self._setup_chat_view()
         
@@ -235,3 +245,8 @@ class ChatPage(Adw.NavigationPage):
     def _show_error(self, message: str):
         """Show error message in chat view"""
         self.message_buffer.show_error(message)
+
+    def _on_browser_clicked(self, button):
+        """Handle browser button click"""
+        window = self.get_root()
+        window.open_stream_in_browser(self.streamer)
